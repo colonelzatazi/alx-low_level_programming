679693e44fca9ce02671d9ca6383258be783562c
ZOUGUE EDMOND <110996501+colonelzatazi@users.noreply.github.com>
2022-10-24 22:42:49 +0000

    fix

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,5 +1,7 @@
-#ifndef LIST
-#define LIST
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,7 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,5 +31,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-void _printd(int n);
-#endif
+
+#endif /* _LISTS_H_ */
